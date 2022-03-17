f0125f7896804f49c70fbd3e080bb2f182ff9450
Robert Boudewijn <robert-boudewijn@hotmail.com>
2022-03-17 09:19:00 +0100

    update fouten van assesment

---
+++ NotS_ChatClient/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace _01_ClientServerChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listChats = new System.Windows.Forms.ListBox();
+            this.txtMessageToBeSend = new System.Windows.Forms.TextBox();
+            this.btnSendMessage = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Disconnect = new System.Windows.Forms.Button();
+            this.txtBufferSize = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnConnectWithServer = new System.Windows.Forms.Button();
+            this.txtChatServerIP = new System.Windows.Forms.TextBox();
+            this.txtServerName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listChats
+            // 
+            this.listChats.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.listChats.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.listChats.FormattingEnabled = true;
+            this.listChats.ItemHeight = 20;
+            this.listChats.Location = new System.Drawing.Point(47, 30);
+            this.listChats.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listChats.Name = "listChats";
+            this.listChats.Size = new System.Drawing.Size(691, 524);
+            this.listChats.TabIndex = 1;
+            // 
+            // txtMessageToBeSend
+            // 
+            this.txtMessageToBeSend.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtMessageToBeSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.txtMessageToBeSend.Location = new System.Drawing.Point(47, 572);
+            this.txtMessageToBeSend.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtMessageToBeSend.Name = "txtMessageToBeSend";
+            this.txtMessageToBeSend.Size = new System.Drawing.Size(592, 26);
+            this.txtMessageToBeSend.TabIndex = 2;
+            // 
+            // btnSendMessage
+            // 
+            this.btnSendMessage.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSendMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.btnSendMessage.Location = new System.Drawing.Point(645, 570);
+            this.btnSendMessage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSendMessage.Name = "btnSendMessage";
+            this.btnSendMessage.Size = new System.Drawing.Size(93, 30);
+            this.btnSendMessage.TabIndex = 3;
+            this.btnSendMessage.Text = "Send";
+            this.btnSendMessage.UseVisualStyleBackColor = true;
+            this.btnSendMessage.Click += new System.EventHandler(this.btnSendMessage_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.Disconnect);
+            this.groupBox2.Controls.Add(this.txtBufferSize);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.btnConnectWithServer);
+            this.groupBox2.Controls.Add(this.txtChatServerIP);
+            this.groupBox2.Controls.Add(this.txtServerName);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.groupBox2.Location = new System.Drawing.Point(754, 30);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(277, 292);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Server Settings";
+            // 
+            // Disconnect
+            // 
+            this.Disconnect.Enabled = false;
+            this.Disconnect.Location = new System.Drawing.Point(134, 237);
+            this.Disconnect.Name = "Disconnect";
+            this.Disconnect.Size = new System.Drawing.Size(116, 30);
+            this.Disconnect.TabIndex = 4;
+            this.Disconnect.Text = "Disconnect";
+            this.Disconnect.UseVisualStyleBackColor = true;
+            this.Disconnect.Click += new System.EventHandler(this.Disconnect_Click);
+            // 
+            // txtBufferSize
+            // 
+            this.txtBufferSize.Location = new System.Drawing.Point(60, 181);
+            this.txtBufferSize.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtBufferSize.Name = "txtBufferSize";
+            this.txtBufferSize.Size = new System.Drawing.Size(190, 26);
+            this.txtBufferSize.TabIndex = 5;
+            this.txtBufferSize.Text = "1024";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 20);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Buffer size";
+            // 
+            // btnConnectWithServer
+            // 
+            this.btnConnectWithServer.Location = new System.Drawing.Point(33, 237);
+            this.btnConnectWithServer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnConnectWithServer.Name = "btnConnectWithServer";
+            this.btnConnectWithServer.Size = new System.Drawing.Size(95, 30);
+            this.btnConnectWithServer.TabIndex = 3;
+            this.btnConnectWithServer.Text = "Connect";
+            this.btnConnectWithServer.UseVisualStyleBackColor = true;
+            this.btnConnectWithServer.Click += new System.EventHandler(this.btnConnectWithServer_Click_1);
+            // 
+            // txtChatServerIP
+            // 
+            this.txtChatServerIP.Location = new System.Drawing.Point(60, 111);
+            this.txtChatServerIP.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtChatServerIP.Name = "txtChatServerIP";
+            this.txtChatServerIP.Size = new System.Drawing.Size(190, 26);
+            this.txtChatServerIP.TabIndex = 3;
+            this.txtChatServerIP.Text = "127.0.0.1";
+            // 
+            // txtServerName
+            // 
+            this.txtServerName.Location = new System.Drawing.Point(60, 56);
+            this.txtServerName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtServerName.Name = "txtServerName";
+            this.txtServerName.Size = new System.Drawing.Size(190, 26);
+            this.txtServerName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "IP";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1054, 634);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnSendMessage);
+            this.Controls.Add(this.txtMessageToBeSend);
+            this.Controls.Add(this.listChats);
+            this.KeyPreview = true;
+            this.Location = new System.Drawing.Point(15, 15);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "NotS Chat - Client";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button Disconnect;
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listChats;
+        private System.Windows.Forms.TextBox txtMessageToBeSend;
+        private System.Windows.Forms.Button btnSendMessage;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtBufferSize;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtChatServerIP;
+        private System.Windows.Forms.TextBox txtServerName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnectWithServer;
+    }
+}
+
---
+++ NotS_ChatClient/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01_ClientServerChat
+{
+    public partial class Form1 : Form
+    {
+        TcpClient tcpClient;
+        NetworkStream networkStream;
+        Thread thread;
+        
+        protected delegate void UpdateDisplayDelegate(string message);
+        protected delegate void ToggleFieldsDelegate();
+        private ToggleFieldsDelegate toggleFields;
+
+        public Form1()
+        {
+            InitializeComponent();
+            toggleFields = () =>
+            {
+                txtBufferSize.Enabled = !txtBufferSize.Enabled;
+                txtServerName.Enabled = !txtServerName.Enabled;
+                txtChatServerIP.Enabled = !txtChatServerIP.Enabled;
+                btnConnectWithServer.Enabled = !btnConnectWithServer.Enabled;
+                Disconnect.Enabled = !Disconnect.Enabled;
+            };
+        }
+        
+        private void AddMessage(string message)
+        {
+            if (listChats.InvokeRequired)
+            {
+                listChats.Invoke(new UpdateDisplayDelegate(UpdateDisplay), new object[] { message });
+            }
+            else
+            {
+                UpdateDisplay(message);
+            }
+        }
+
+        private void UpdateDisplay(string message)
+        {
+            listChats.Items.Add(message);
+        }
+        
+        private void ReceiveData()
+        {
+            int bufferSize;
+            int ignoreMe;
+            bool succes = int.TryParse(txtBufferSize.Text, out ignoreMe);
+            if (succes && int.Parse(txtBufferSize.Text) >=1 && int.Parse(txtBufferSize.Text) <=25565 )            {
+                bufferSize = int.Parse(txtBufferSize.Text);
+            }
+            else 
+            {
+                bufferSize = 1024;
+                AddMessage("Foutmelding: Buffersize moet een positief interger nummer zijn en kleiner dan 25565. We hebben de buffer size aangepast naar 1024.");
+                txtBufferSize.Text = "1024";
+            }
+            StringBuilder SB = new StringBuilder();
+            byte[] buffer = new byte[bufferSize];
+
+            networkStream = tcpClient.GetStream();
+            AddMessage("Connected!");
+
+            while (true)
+            {
+
+                string partMsg = "";
+                try
+                {
+                    while (networkStream.DataAvailable){
+                        int readBytes = networkStream.Read(buffer, 0, buffer.Length);
+                        partMsg = Encoding.ASCII.GetString(buffer, 0, readBytes);
+                        SB.Append(partMsg);
+                        // clear buffer:
+                        buffer = new byte[bufferSize];
+                    
+                    }
+                }
+                catch (ObjectDisposedException e)
+                {
+                    /*
+                     * due to an race condition,
+                     * we will catch if the object doesnt exist anymore.
+                     * And abort the thread, clearing everything 
+                     */
+                    thread.Abort();
+                    AddMessage("Connection closed");
+                }
+                catch (Exception exception)
+                {
+                    AddMessage("Foutmelding: Er is iets fout gegaan: " + exception);
+                    Console.WriteLine("Exception: ", exception);
+                }
+
+                if (SB.ToString().Contains("SERVER SAYS BYE"))
+                    break;
+                
+                if (SB.ToString() != "")
+                {
+                    AddMessage(SB.ToString());
+                    SB.Clear();
+                }
+            }
+
+            // Verstuur een reactie naar de client (afsluitend bericht)
+            buffer = Encoding.ASCII.GetBytes("[" + txtServerName.Text+ "]: bye");
+            networkStream.Write(buffer, 0, buffer.Length);
+
+            // cleanup:
+            networkStream.Close();
+            tcpClient.Close();
+
+            AddMessage("Connection closed");
+            toggleFields();
+        }
+        
+        private void btnConnectWithServer_Click_1(object sender, EventArgs e)
+        {
+            if (txtServerName.Text == "")
+            {
+                AddMessage("Foutmelding: Je moet een gebruikersnaam invullen.");
+                return;
+            }
+            toggleFields();
+            AddMessage("Connecting...");
+            try
+            {
+                tcpClient = new TcpClient(txtChatServerIP.Text, 9000);
+                thread = new Thread(new ThreadStart(ReceiveData));
+                thread.Start();
+            }
+            catch (SocketException exception)
+            {
+                AddMessage("Foutmelding: We konden op: " + txtChatServerIP.Text + ". Geen server vinden.");
+                Console.WriteLine("Exception: ", exception);
+                toggleFields();
+            }
+            catch (Exception exception)
+            {
+                AddMessage("Foutmelding: Er is iets fout gegaan: " + exception);
+                Console.WriteLine("Exception: ", exception);
+                toggleFields();
+            }
+        }
+        
+        private void btnSendMessage_Click(object sender, EventArgs e)
+        {
+            string message = "[" + txtServerName.Text+ "]: " + txtMessageToBeSend.Text;
+
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            try
+            {
+                networkStream.Write(buffer, 0, buffer.Length);
+
+                // AddMessage(message);
+                txtMessageToBeSend.Clear();
+                txtMessageToBeSend.Focus();
+            }
+            catch (NullReferenceException exception)
+            {
+                AddMessage("Foutmelding: Je moet wel verbonden zijn met een server om een bericht te verzenden.");
+                Console.WriteLine("Exception: ", exception);
+            }
+            catch (ObjectDisposedException exception)
+            {
+                AddMessage("Foutmelding: Je moet wel verbonden zijn met een server om een bericht te verzenden.");
+                Console.WriteLine("Exception: ", exception);
+            }
+            catch (Exception exception)
+            {
+                AddMessage("Foutmelding: Er is iets fout gegaan: " + exception);
+                Console.WriteLine("Exception: ", exception);
+            }
+
+        }
+
+        private void Disconnect_Click(object sender, EventArgs e)
+        {
+            string message = "CLIENT SAYS GOODBYE";
+
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            try
+            {
+                networkStream.Write(buffer, 0, buffer.Length);
+                
+                txtMessageToBeSend.Clear();
+                txtMessageToBeSend.Focus();
+                // cleanup:
+                networkStream.Close();
+                tcpClient.Close();
+
+                toggleFields();
+                
+            }
+            catch (NullReferenceException exception)
+            {
+                AddMessage("Foutmelding: Je moet wel verbonden zijn met een server om een bericht te verzenden.");
+                Console.WriteLine("Exception: ", exception);
+            }
+            catch (Exception exception)
+            {
+                AddMessage("Foutmelding: Er is iets fout gegaan: " + exception);
+                Console.WriteLine("Exception: ", exception);
+            }
+        }
+    }
+}
